ffd61c0b3908d804910d6fe5f9db89cb662d8d7f
Garrett Maggio <garrett.maggio@selu.edu>
2025-09-16 14:15:20 -0500

    Added in the insertion sort algorithm

--- Structs and functions/algorithms.c
+++ Structs and functions/algorithms.c
@@ -3,9 +3,9 @@
 void selectionSort(int *array, int size) {
 
     for (int i = 0; i < size; i++) {
-        int min = i;
-        for (int j = i+1; j < size; j++) {
-            if (array[j] < array[min]) {
+        int min = i; // first iteration i = 0 first element in array
+        for (int j = i+1; j < size; j++) { // loops through the array until it finds a value that is smaller
+            if (array[j] < array[min]) {         // or the end of the array
                 int temp = array[min];
                 array[min] = array[j];
                 array[j] = temp;
@@ -16,10 +16,32 @@ void selectionSort(int *array, int size) {
     printf("\n");
 }
 
+void insertionSort(int *array, int size) {
+
+    for (int i = 1; i < size; i++) {
+        int temp = array[i];
+        int x = i - 1;
+
+        while (array[x] > temp && x >= 0) {
+            array[x] = temp;
+            x = x - 1;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf ("%d ", array[i]);
+    }
+    printf("\n");
+
+}
+
 
 int algorithms() {
 
     int unsorted_array[] = {4, 2, 6, 9, 1, 3, 8, 7, 5, 10};
+
+    int size = sizeof(unsorted_array) / sizeof(unsorted_array[0]); // this is the same as saying sizeof(array) / 4 because int's occupy 4 bytes of memory
+                                                                    // so 4 bytes per element.
 
     printf("%s\n", "Unsorted array");
     for (int i = 0; i < sizeof(unsorted_array) / sizeof(unsorted_array[0]); i++) {
@@ -28,9 +50,10 @@ int algorithms() {
     printf("\n");
 
     printf("%s\n", "Selection Sort");
-    selectionSort(unsorted_array, sizeof(unsorted_array)/sizeof(unsorted_array[0])); // this is the same as
-                                                                                            // saying sizeof(unsorted_array) / 4
-                                                                                            // in this case because the elements are int's which occupy 4 bytes each.
+    selectionSort(unsorted_array, size);
+
+    printf("%s\n", "Insertion Sort");
+    insertionSort(unsorted_array, size);
 
     return 0;
 
